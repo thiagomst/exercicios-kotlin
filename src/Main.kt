6e5f780ee460cbdb14e4b7faa5774f8837b848bf
thiagomst <120652397+thiagomst@users.noreply.github.com>
2025-04-15 21:59:32 -0300

    15

--- src/Main.kt
+++ src/Main.kt
@@ -19,22 +19,39 @@
 //um programa que pergunte a quantidade de Km percorridos por um carro alugado e a
 //quantidade de dias pelos quais ele foi alugado. Calcule o preço total a pagar,
 //sabendo que o carro custa R$90 por dia e R$0,20 por Km rodado.
+//
+//    fun atividade14(){
+//        println("Informe aqui a quantidade de Km percorridos pelo carro que foi alugado: ")
+//        val km = readln().toDouble()
+//        println("Agora informe quantos dias o carro ficou alugado por você: ")
+//        val aluguel = readln().toInt()
+//        val diaria = 90
+//        val kmRodado = 0.20
+//
+//        val valorDiaria = aluguel * diaria
+//        val valorKm = km * kmRodado
+//
+//        println("Vovê vai pagar no total de R$ ${valorDiaria}  pelo aluguel do carro ")
+//        println("E para finaliar você vai pagar + R$ ${valorKm}, por KM rodado. " )
+//}
 
-    fun atividade14(){
-        println("Informe aqui a quantidade de Km percorridos pelo carro que foi alugado: ")
-        val km = readln().toDouble()
-        println("Agora informe quantos dias o carro ficou alugado por você: ")
-        val aluguel = readln().toInt()
-        val diaria = 90
-        val kmRodado = 0.20
+//15) Crie um programa que leia o número de dias trabalhados em um mês e mostre o
+//salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25
+//por hora trabalhada.
 
-        val valorDiaria = aluguel * diaria
-        val valorKm = km * kmRodado
+fun atividade15(){
+    println("Informe aqui o número de dias trabalhado em um mês: ")
+    val diasTrabalhado = readln().toInt()
+    val horasPorDia = 8
+    val horaTrabalhada = 25
 
-        println("Vovê vai pagar no total de R$ ${valorDiaria}  pelo aluguel do carro ")
-        println("E para finaliar você vai pagar + R$ ${valorKm}, por KM rodado. " )
+    val salarioHora = diasTrabalhado * horasPorDia * horaTrabalhada
+
+    println("O salário total no mês é de R$ ${"%.2f".format(salarioHora.toDouble())}")
 }
+
 fun main() {
     //atividade13()
-    atividade14()
+    //atividade14()
+    atividade15()
 }
