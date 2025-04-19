1381fef923358fa76812af9a3cb76ba3e745c1f0
thiagomst <120652397+thiagomst@users.noreply.github.com>
2025-04-18 21:44:59 -0300

    Adicionar novos exercícios

--- src/Main.kt
+++ src/Main.kt
@@ -101,32 +101,107 @@
 //    val nacimento = readln().toInt()
 //
 //    val idade = 2025 - nacimento
-//    if (idade < 18 ){
-//        println("Você não pode votar pois ainda é menor de idade $idade anos.")
-//    }else {
+//    if (idade >= 18 ){
 //        println("Você já pode votar pois é maior de idade $idade anos.")
+//    }else {
+//        println("Você não pode votar pois ainda é menor de idade $idade anos.")
 //    }
 //}
 
 //19) Crie um algoritmo que leia o nome e as duas notas de um aluno, calcule a sua
 //média e mostre na tela. No final, analise a média e mostre se o aluno teve ou
 //não um bom aproveitamento (se ficou acima da média 7.0).
+//
+//fun atividade19(){
+//    println("Informe aqui o seu nome: ")
+//    val nome = readln()
+//
+//    println("Informe o valor da primeira nota: ")
+//    val notaA = readln().toDouble()
+//    println("Informe o valor da segunda nota: ")
+//    val notaB = readln().toDouble()
+//
+//    val media = (notaA + notaB) / 2
+//
+//    if (media >= 7) {
+//        println("Parabéns ${nome}, pois sua média é ${"%.2f".format(media)} e está acima da média.")
+//    } else {
+//        println("${nome}, você ficou abaixo da média. Sua média foi ${"%.2f".format(media)}.")
+//    }
+//}
 
-fun atividade19(){
-    println("Informe aqui o seu nome: ")
+//20) Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou
+//ÍMPAR.
+
+//fun atividade20(){
+//    println("Digiti aqui um número inteiro para saber se ele é PAR ou ÍMPAR: ")
+//    val numero = readln().toInt()
+//
+//    if (numero  % 2 == 0){
+//        println("O número ${numero} é Par")
+//    }else{
+//        println("O número ${numero} é Impar")
+//    }
+//}
+
+//21) Faça um algoritmo que leia um determinado ano e mostre se ele é ou não
+////BISSEXTO.
+//
+//fun atividade21(){
+//    println("Digite aqui um determinando ano para ver se ele é ou não Bissexto. ")
+//    val ano = readln().toInt()
+//
+//    val anoBissexto = (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)
+//
+//    if (anoBissexto){
+//        println("O ano de ${ano}, é Bissexto e tem 366 dias")
+//    }else{
+//        println("O ano de ${ano}, não é um ano Bissexto e tem 365 dias. ")
+//    }
+//}
+
+//22) Escreva um programa que leia o ano de nascimento de um rapaz e mostre a sua
+//situação em relação ao alistamento militar. - Se estiver antes dos 18 anos, mostre em quantos anos faltam para o
+//alistamento. - Se já tiver depois dos 18 anos, mostre quantos anos já se passaram do
+//alistamento.
+//
+//fun atividade22(){
+//    println("Informe aqui a Data do seu nascimento: ")
+//    val nacimento = readln().toInt()
+//
+//    val idade = 2025 - nacimento
+//
+//    if (idade < 18){
+//        println("Você ainda não pode se alista pois tem ${idade} anos de idade, esta faltando ${18 - idade} anos. ")
+//    }else if (idade == 18 ){
+//        println("Você já pode se alista pois ja completou ${idade} anos de idade.")
+//    }else {
+//        println("Você já tem ${idade} anos de idade, e ja se passaram ${idade - 18} anos. ")
+//    }
+//
+//}
+
+//23) Numa promoção exclusiva para o Dia da Mulher, uma loja quer dar descontos
+//para todos, mas especialmente para mulheres. Faça um programa que leia nome,
+//sexo e o valor das compras do cliente e calcule o preço com desconto. Sabendo
+//que: - Homens ganham 5% de desconto - Mulheres ganham 13% de desconto
+
+fun atividade23(){
+    println("Infomre aqui o seu nome: ")
     val nome = readln()
+    println("Informe agora F para o sexo (Feminino) ou M para (Masculino): ")
+    val sexo = readln()
+    println("Informe aqui o valor das suas compras: ")
+    val valor = readln().toDouble()
 
-    println("Informe o valor da primeira nota: ")
-    val notaA = readln().toDouble()
-    println("Informe o valor da segunda nota: ")
-    val notaB = readln().toDouble()
-
-    val media = (notaA + notaB) / 2
-
-    if (media >= 7) {
-        println("Parabéns ${nome}, pois sua média é ${"%.2f".format(media)} e está acima da média.")
-    } else {
-        println("${nome}, você ficou abaixo da média. Sua média foi ${"%.2f".format(media)}.")
+    if(sexo == "F"){
+        val desconto =  valor * 0.13
+        val valorFinal = valor - desconto
+        println("${nome}, Voce gastou R$ ${"%.2f".format(valor)} e teve um desconto de R$ ${"%.2f".format(desconto)} e vai pagar R$ ${"%.2f".format(valorFinal)}")
+    }else if ( sexo == "M"){
+        val desconto =  valor * 0.05
+        val valorFinal = valor - desconto
+        println("${nome}, Voce gastou R$ ${"%.2f".format(valor)} e teve um desconto de R$ ${"%.2f".format(desconto)} e vai pagar R$ ${"%.2f".format(valorFinal)}")
     }
 }
 
@@ -137,5 +212,9 @@ fun main() {
     //atividade16()
     //atividade17()
     //atividade18()
-    atividade19()
+    //atividade19()
+    //atividade20()
+    //atividade21()
+    //atividade22()
+    atividade23()
 }
